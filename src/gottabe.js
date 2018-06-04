bc1ae56ed4e7623b61ad7323683f63b6736593af
alan.lohse <alan.lohse@novatendencia.localdomain>
2018-06-04 15:22:32 -0300

    Tests running.

--- src/gottabe.js
+++ src/gottabe.js
@@ -71,7 +71,7 @@ if (!(commands.clean || commands.build || commands.package || commands.install |
 const fs = require('fs');
 const utils = require('./utils.js');
 const rjson = require("relaxed-json");
-const Set = require('es6-set');
+const path = require('path');
 
 const BUILD_FILE = './build.json';
 
@@ -87,10 +87,6 @@ if (!fs.existsSync(BUILD_FILE)) {
 var data = fs.readFileSync(BUILD_FILE);
 data = rjson.transform(data.toString());
 build = JSON.parse(data);
-
-function uniqueArray(array) {
-    return Array.from(new Set(array));
-}
 
 var defaultBuild = {
     name: '',
@@ -115,11 +111,11 @@ var defaultTarget = {
     toolchain: '',
     includeDirs: [],
     sources: [],
-    options: {optimization : 0, debug : 3, warnings : 'all', other :''},
+    options: { optimization: 0, debug: 3, warnings: 'all', other: '' },
     defines: {},
     libraryPaths: [],
     libraries: [],
-    linkoptions: {debugInformation : true}
+    linkoptions: { debugInformation: true }
 };
 
 build = Object.assign(defaultBuild, build);
@@ -162,7 +158,8 @@ function findTarget(build, arch, plat, toolchain) {
 determinetarget();
 
 commands.package |= commands.install;
-commands.build |= commands.package;
+commands.test |= commands.package && !program.noTests;
+commands.build |= commands.package || commands.test;
 
 if (commands.clean) {
 
@@ -179,95 +176,46 @@ if (commands.clean) {
 
 if (commands.build) {
 
-    const tool = require('./' + target.toolchain + '.js');
-    const install = require('./dependencies.js');
+    const buildjs = require('./build');
 
-    var sources = build.sources;
-    if (target.sources)
-        sources = sources.concat(target.sources);
+    buildjs(target, arch, plat, build, {}, function() {
+        if (commands.test) {
 
-    var sourceFiles = [];
+            if (build.testSources && build.testSources.length > 0) {
 
-    sources.forEach(path => utils.getFiles(path, sourceFiles));
+                const { execSync } = require('child_process');
 
-    var destFolder = './build/' + target.name + '/';
+                var test_build = Object.assign(build, {
+                    type: 'executable',
+                    sources: build.testSources.concat(build.sources),
+                    package: {
+                        name: build.package.name + '_test'
+                    }
+                });
 
-    var destFiles = sourceFiles.map(function(src) {
-        return destFolder + src.replace(/^.*?\/([a-z0-9_~-]+)\.[a-z0-9_~-]+$/i, '$1');
-    });
+                var tests_failed = false;
 
-    const { execSync } = require('child_process');
+                buildjs(target, arch, plat, test_build, {}, function(cmdTest) {
+                    try {
+                        execSync(path.normalize(cmdTest));
+                    } catch (e) {
+                        if (e.status != 0) {
+                            tests_failed = true;
+                            console.log('Tests failed: ' + e.message);
+                        }
+                    }
+                }, true);
 
-    if (!fs.existsSync('./build')) {
-        fs.mkdirSync('./build');
-    }
-
-    if (!fs.existsSync('./build/' + target.name)) {
-        fs.mkdirSync('./build/' + target.name);
-    }
-
-    if (!fs.existsSync(destFolder + 'bin')) {
-        fs.mkdirSync(destFolder + 'bin');
-    }
-
-    var hasErrors = false;
-
-    var packages = [];
-    build.dependencies.forEach(dep => {
-        packages.push(install.getPackage(dep, arch, plat, target.toolchain, packages, build.servers.concat(config.servers).concat(defaultServers)));
-    });
-    var includeDeps = uniqueArray(packages.map(pack => pack.includeDir));
-
-    var promisses = [true];
-    packages.forEach(pack => {
-        promisses = promisses.concat(pack.promisses);
-    });
-
-    Promise.all(promisses)
-            .then(a => {
-        destFolder = './build/' + target.name + '/';
-        for (var i = 0; i < sourceFiles.length; i++) {
-            if (!utils.isOutdated(sourceFiles[i], destFiles[i] + '.' + tool.OBJECT_EXTENSION))
-                continue;
-            var cmd = tool.compile(sourceFiles[i], destFiles[i], build.includeDirs.concat(target.includeDirs).concat(includeDeps), target.defines, target.options);
-            console.log(cmd);
-            try {
-                execSync(cmd);
-            } catch (e) {
-                hasErrors = true;
-                console.log(e.output.toString());
+                if (tests_failed) {
+                    process.exit(1);
+                }
+                console.log('Tests finished with success.');
+            } else {
+                console.log('No tests to build.');
             }
         }
+    });
 
-        if (hasErrors) {
-            console.error('Errors found when building ' + target.name);
-            process.exit(1);
-        }
-
-        var artifactName = tool.artifactName(build, target);
-
-        var apt = arch + '_' + plat + '_' + target.toolchain;
-        // get the library paths and libraries from dependencies
-        var libraryPathDeps = uniqueArray(packages.map(pack => pack[apt])), 
-            libraryDeps = packages.map(pack => pack.build.package.name);
-
-        // get all libraries from dependencies for the target
-        packages.forEach(pack => {
-            var target2 = findTarget(pack.build, arch, plat, target.toolchain);
-            libraryDeps = libraryDeps.concat(target2.libraries);
-        });
-
-        if (utils.isOutdated(destFiles.map(s => s + '.' + tool.OBJECT_EXTENSION), destFolder + 'bin/' + artifactName)) {
-            var cmd = tool.link(build.type, destFiles, destFolder + 'bin/' + artifactName, target.libraryPaths.concat(libraryPathDeps), 
-                    uniqueArray(libraryDeps.concat(target.libraries)), target.linkoptions)
-            console.log(cmd);
-            try {
-                execSync(cmd);
-            } catch (e) {
-                console.log(e.output.toString());
-            }
-        }
-    }).catch(err => console.error(err));
 } // end of build command
 
 if (commands.package) {
